32a1e2dff73eeeb8566024818b46e4ca1a029dee
Brandon Borkholder <borkholder@metsci.com>
2012-03-20 23:09:49 -0400

    Changed to using the pkcs12 format for the keystore and also using the same key as the glimpse.metsci.com server.

--- assembly/assembly-webstart-examples/assemble.sh
+++ assembly/assembly-webstart-examples/assemble.sh
@@ -2,7 +2,7 @@
 ASSEMBLY_DIR=`dirname "$0"`
 
 # check for the keystore
-[ -f "$ASSEMBLY_DIR/glimpse.keystore" ] || ( echo Must have keystore at glimpse.keystore && exit )
+[ -f "$ASSEMBLY_DIR/glimpse-server-keystore.pkcs12" ] || ( echo Must have keystore at glimpse.keystore && exit )
 
 # check for password argument
 [ -z "$1" ] && echo Must provide keystore password as first argument && exit
