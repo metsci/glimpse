99d80e45a12d6de873588a86e4f75924eea21bd6
Geoffrey Ulman <ulman@metsci.com>
2013-09-23 23:03:12 -0400

    Further pom version updates.

--- assembly/assembly-full/assemble.bat
+++ assembly/assembly-full/assemble.bat
@@ -1,4 +1,4 @@
 @echo off
 set ASSEMBLY_DIR=%~dp0
 if "%ASSEMBLY_DIR:~-1%" == "\" set ASSEMBLY_DIR=%ASSEMBLY_DIR:~0,-1%
-mvn -f "%ASSEMBLY_DIR%\..\..\pom.xml" clean install && mvn -f "%ASSEMBLY_DIR%\pom.xml" clean package
+mvn -U -f "%ASSEMBLY_DIR%\..\..\pom.xml" clean install && mvn -U -f "%ASSEMBLY_DIR%\pom.xml" clean package
