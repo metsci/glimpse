772550e4ff4317505c06e7659a1f1ca963d8c198
Mike Hogye <hogye@metsci.com>
2014-07-11 00:10:36 -0400

    Squash annoying warnings when building native Windows fixes

--- platform-fixes/natives/windows/build.bat
+++ platform-fixes/natives/windows/build.bat
@@ -1,11 +1,11 @@
 @echo off
 
-mkdir build
+if not exist build mkdir build
 javah -classpath ../../target/classes -d build com.metsci.glimpse.platformFixes.WindowsFixes
 
 
 
-mkdir build\64bit
+if not exist build\64bit mkdir build\64bit
 
 x86_64-w64-mingw32-g++ -std=c++11 -I".\build" -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -c windowsFixes.cc -o build\64bit\windowsFixes.o
 x86_64-w64-mingw32-g++ -shared build\64bit\windowsFixes.o -o build\64bit\windowsFixes.dll
@@ -21,7 +21,7 @@ copy /y "%LIBGCC_64:/=\%" ..\..\src\main\resources\platformFixes\windows64
 
 
 
-mkdir build\32bit
+if not exist build\32bit mkdir build\32bit
 
 i686-w64-mingw32-g++ -std=c++11 -I".\build" -I"%JAVA_HOME%\include" -I"%JAVA_HOME%\include\win32" -c windowsFixes.cc -o build\32bit\windowsFixes.o
 i686-w64-mingw32-g++ -shared build\32bit\windowsFixes.o -o build\32bit\windowsFixes.dll
