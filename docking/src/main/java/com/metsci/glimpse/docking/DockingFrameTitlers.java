ade923c43dc575ac68226d807586fd675d92b275
Mike Hogye <hogye@metsci.com>
2014-07-14 09:29:10 -0400

    Added missing override annotations

--- docking/src/main/java/com/metsci/glimpse/docking/DockingFrameTitlers.java
+++ docking/src/main/java/com/metsci/glimpse/docking/DockingFrameTitlers.java
@@ -43,51 +43,61 @@ public class DockingFrameTitlers
             }
         }
 
+        @Override
         public void addedView( Tile tile, View view )
         {
             updateFrameTitle( getAncestorOfClass( DockingFrame.class, tile ) );
         }
 
+        @Override
         public void removedView( Tile tile, View view )
         {
             updateFrameTitle( getAncestorOfClass( DockingFrame.class, tile ) );
         }
 
+        @Override
         public void selectedView( Tile tile, View view )
         {
             updateFrameTitle( getAncestorOfClass( DockingFrame.class, tile ) );
         }
 
+        @Override
         public void addedLeaf( MultiSplitPane docker, Component leaf )
         {
             updateFrameTitle( getAncestorOfClass( DockingFrame.class, docker ) );
         }
 
+        @Override
         public void removedLeaf( MultiSplitPane docker, Component leaf )
         {
             updateFrameTitle( getAncestorOfClass( DockingFrame.class, docker ) );
         }
 
+        @Override
         public void movedDivider( MultiSplitPane docker, SplitPane splitPane )
         {
             updateFrameTitle( getAncestorOfClass( DockingFrame.class, docker ) );
         }
 
+        @Override
         public void maximizedLeaf( MultiSplitPane docker, Component leaf )
         {
             updateFrameTitle( getAncestorOfClass( DockingFrame.class, docker ) );
         }
 
+        @Override
         public void unmaximizedLeaf( MultiSplitPane docker, Component leaf )
         {
             updateFrameTitle( getAncestorOfClass( DockingFrame.class, docker ) );
         }
 
+        @Override
         public void restoredTree( MultiSplitPane docker )
         {
             updateFrameTitle( getAncestorOfClass( DockingFrame.class, docker ) );
         }
 
+        @Override
         public void addedFrame( DockingGroup group, DockingFrame frame )
         {
             updateFrameTitle( frame );
