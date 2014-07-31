56b33bb18a4f1fb5ebfc52a52d3bd06e9464d038
Mike Hogye <hogye@metsci.com>
2014-07-30 22:42:37 -0400

    Make maximize-tile and close-view buttons unfocusable

--- docking/src/main/java/com/metsci/glimpse/docking/TileFactories.java
+++ docking/src/main/java/com/metsci/glimpse/docking/TileFactories.java
@@ -70,6 +70,7 @@ public class TileFactories
             final DockingTheme theme = dockingGroup.theme;
 
             final Tile[] tileRef = { null };
+
             final JButton maximizeButton = new JButton( theme.maximizeIcon )
             {
                 public void paintComponent( Graphics g )
@@ -81,6 +82,7 @@ public class TileFactories
                     super.paintComponent( g );
                 }
             };
+            maximizeButton.setFocusable( false );
 
             TabComponentFactory tabCornerComponentFactory = new TabComponentFactory( )
             {
@@ -89,6 +91,7 @@ public class TileFactories
                     if ( view.closeable )
                     {
                         JButton closeButton = new JButton( );
+                        closeButton.setFocusable( false );
                         closeButton.setOpaque( false );
                         closeButton.setBorder( createEmptyBorder( 2, 0, 0, theme.lineThickness + theme.labelPadding ) );
 
