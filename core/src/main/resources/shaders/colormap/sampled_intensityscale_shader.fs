fd2ddbab0fb342261e0c9edfffa086a5b48e612f
Ben Sherman <sherman@metsci.com>
2014-12-18 11:42:19 -0500

    Fix clamping of value value for intensity map painter

--- core/src/main/resources/shaders/colormap/sampled_intensityscale_shader.fs
+++ core/src/main/resources/shaders/colormap/sampled_intensityscale_shader.fs
@@ -12,13 +12,12 @@ void main()
     float hueVal = texture2D( huetex, gl_TexCoord[0].st ).r;
     float dataVal = texture2D( datatex, gl_TexCoord[0].st ).r;
     float normalizedVal = ( dataVal - dataMin ) / ( dataMax - dataMin );
-    clamp( normalizedVal, 0.0, 1.0 );
+    float nVal = clamp( normalizedVal, 0.0, 1.0 );
 
-    vec3 hsv = vec3(hueVal, 1.0, normalizedVal);
-    
+    vec3 HSV = vec3(hueVal, 1.0, nVal);
     vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-    vec3 p = abs(fract(hsv.xxx + K.xyz) * 6.0 - K.www);
-    vec3 color = hsv.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), hsv.y);
+    vec3 p = abs(fract(HSV.xxx + K.xyz) * 6.0 - K.www);
+    vec3 color = HSV.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), HSV.y);
 
     gl_FragColor.rgb = color;
     gl_FragColor.a = alpha;
