1eb42abce409f5237fe098966869d8190f0b252a
Oren Bumgarner <bumgarner@metsci.com>
2016-05-16 17:02:03 -0400

    cleanup/formatting before merging

--- extras-charts/src/main/java/com/metsci/glimpse/charts/slippy/SlippyMapPainter.java
+++ extras-charts/src/main/java/com/metsci/glimpse/charts/slippy/SlippyMapPainter.java
@@ -6,49 +6,64 @@ import com.metsci.glimpse.painter.group.DelegatePainter;
 import com.metsci.glimpse.painter.info.SimpleTextPainter;
 import com.metsci.glimpse.support.font.FontUtils;
 
-public class SlippyMapPainter extends DelegatePainter {
+/**
+ * This just wraps a tile painter and an attribution text painter (if one is specified).
+ * 
+ * @author oren
+ *
+ */
+public class SlippyMapPainter extends DelegatePainter
+{
 
     protected final SlippyMapTilePainter tilePainter;
     protected final String attribution;
-    private SimpleTextPainter attributionPainter;
-    
+    protected final SimpleTextPainter attributionPainter;
+
     private static final Font textFont = FontUtils.getDefaultPlain( 15.0f );
     private static final float[] textColor = new float[] { 0.5f, 0.5f, 0.5f, 1.0f };
     private static final float[] bgColor = new float[] { 0.95f, 0.95f, 0.95f, 1f };
 
-    public SlippyMapPainter(SlippyMapTilePainter tilePainter) {
-        this(tilePainter, null);
+    public SlippyMapPainter( SlippyMapTilePainter tilePainter )
+    {
+        this( tilePainter, null );
     }
-    
-    public SlippyMapPainter(SlippyMapTilePainter tilePainter, String attribution) {
-        super();
+
+    public SlippyMapPainter( SlippyMapTilePainter tilePainter, String attribution )
+    {
+        super( );
         this.tilePainter = tilePainter;
         this.attribution = attribution;
-        addPainter(tilePainter);
-        if (attribution != null) {
-            attributionPainter = createAttributionPainter(attribution);
-            addPainter(attributionPainter);
-        } else {
+        addPainter( tilePainter );
+        if ( attribution != null )
+        {
+            attributionPainter = createAttributionPainter( attribution );
+            addPainter( attributionPainter );
+        }
+        else
+        {
             attributionPainter = null;
         }
     }
 
-    public static SimpleTextPainter createAttributionPainter(String text) {
-        SimpleTextPainter painter = new SimpleTextPainter();
-        painter.setText(text);
-        painter.setFont(textFont);
-        painter.setColor(textColor);
-        painter.setBackgroundColor(bgColor);
-        painter.setPaintBackground(true);
+    public static SimpleTextPainter createAttributionPainter( String text )
+    {
+        SimpleTextPainter painter = new SimpleTextPainter( );
+        painter.setText( text );
+        painter.setFont( textFont );
+        painter.setColor( textColor );
+        painter.setBackgroundColor( bgColor );
+        painter.setPaintBackground( true );
         return painter;
     }
-    
-    public SlippyMapTilePainter getTilePainter() {
+
+    public SlippyMapTilePainter getTilePainter( )
+    {
         return tilePainter;
     }
-    
-    public SimpleTextPainter getAttributionPainter() {
+
+    public SimpleTextPainter getAttributionPainter( )
+    {
         return attributionPainter;
     }
-    
+
 }
--- extras-charts/src/main/java/com/metsci/glimpse/charts/slippy/SlippyProjection.java
+++ extras-charts/src/main/java/com/metsci/glimpse/charts/slippy/SlippyProjection.java
@@ -11,42 +11,48 @@ import static java.lang.Math.PI;
  * See http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Java
  * @author oren
  */
-public class SlippyProjection implements GeoProjection {
+public class SlippyProjection implements GeoProjection
+{
 
-    @SuppressWarnings("unused")
+    @SuppressWarnings( "unused" )
     private final int zoom;
     private final double zoomFac;
-    
-    public SlippyProjection(int zoom) {
+
+    public SlippyProjection( int zoom )
+    {
         this.zoom = zoom;
-        this.zoomFac = 1<<zoom;
-    }
-    
-    @Override
-    public Vector2d project(LatLonGeo llg) {
-        double lonDeg = llg.getLonDeg();
-        double latRad = llg.getLatRad();
-        double x =  (lonDeg + 180) / 360 * zoomFac ;
-        double y =  (1 - Math.log(Math.tan(latRad) + 1 / Math.cos(latRad)) / PI) / 2 * zoomFac;
-        return new Vector2d(x, y);
+        this.zoomFac = 1 << zoom;
     }
 
     @Override
-    public LatLonGeo unproject(double x, double y) {
+    public Vector2d project( LatLonGeo llg )
+    {
+        double lonDeg = llg.getLonDeg( );
+        double latRad = llg.getLatRad( );
+        double x = ( lonDeg + 180 ) / 360 * zoomFac;
+        double y = ( 1 - Math.log( Math.tan( latRad ) + 1 / Math.cos( latRad ) ) / PI ) / 2 * zoomFac;
+        return new Vector2d( x, y );
+    }
+
+    @Override
+    public LatLonGeo unproject( double x, double y )
+    {
         double lon = x / zoomFac * 360.0 - 180;
-        double n = PI - (2.0 * PI * y) /zoomFac;
-        double lat = Math.toDegrees(Math.atan(Math.sinh(n)));
-        return LatLonGeo.fromDeg(lat, lon);
+        double n = PI - ( 2.0 * PI * y ) / zoomFac;
+        double lat = Math.toDegrees( Math.atan( Math.sinh( n ) ) );
+        return LatLonGeo.fromDeg( lat, lon );
     }
 
     @Override
-    public Vector2d reprojectFrom(double x, double y, GeoProjection fromProjection) {
-        return project(fromProjection.unproject(x, y));
+    public Vector2d reprojectFrom( double x, double y, GeoProjection fromProjection )
+    {
+        return project( fromProjection.unproject( x, y ) );
     }
 
     @Override
-    public KinematicVector2d reprojectPosVelFrom(double x, double y, double vx, double vy, GeoProjection fromProjection) {
-        throw new UnsupportedOperationException();
+    public KinematicVector2d reprojectPosVelFrom( double x, double y, double vx, double vy, GeoProjection fromProjection )
+    {
+        throw new UnsupportedOperationException( );
     }
-    
+
 }
