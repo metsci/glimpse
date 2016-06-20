243afb5d37981b214f23aaa6c8bf7d54d4f92fcd
Mike Hogye <hogye@metsci.com>
2016-06-20 13:16:41 -0400

    Finish off DncDataPaths

--- extras-dnc/src/main/java/com/metsci/glimpse/dnc/DncDataPaths.java
+++ extras-dnc/src/main/java/com/metsci/glimpse/dnc/DncDataPaths.java
@@ -29,8 +29,13 @@ package com.metsci.glimpse.dnc;
 import static com.metsci.glimpse.util.GlimpseDataPaths.glimpseSharedDataDir;
 import static com.metsci.glimpse.util.GlimpseDataPaths.glimpseUserCacheDir;
 import static com.metsci.glimpse.util.GlimpseDataPaths.glimpseUserDataDir;
+import static com.metsci.glimpse.util.logging.LoggerUtils.getLogger;
+import static com.metsci.glimpse.util.logging.LoggerUtils.logInfo;
 
 import java.io.File;
+import java.util.logging.Logger;
+
+import com.metsci.glimpse.util.GlimpseDataPaths;
 
 public class DncDataPaths
 {
@@ -38,83 +43,107 @@ public class DncDataPaths
     /**
      * Most code should NOT use this, but should use {@link DncDataPaths#glimpseDncFlatDir} instead.
      *
-     * This is provided for programs that, for some reason, need to bypass the ( userDir || sharedDir )
-     * fallback behavior and  access the shared dir specifically.
-     */
-    public static final File glimpseDncDefaultSharedFlatDir = new File( glimpseSharedDataDir, "DNC_FLAT" );
-
-    /**
-     * Most code should NOT use this, but should use {@link DncDataPaths#glimpseDncFlatDir} instead.
-     *
-     * This is provided for programs that, for some reason, need to bypass the ( userDir || sharedDir )
-     * fallback behavior and  access the user dir specifically.
+     * This is provided for code that, for some reason, needs to bypass the ( userDir || sharedDir )
+     * fallback behavior and access the user dir specifically.
      */
     public static final File glimpseDncDefaultUserFlatDir = new File( glimpseUserDataDir, "DNC_FLAT" );
 
     /**
+     * Most code should NOT use this, but should use {@link DncDataPaths#glimpseDncFlatDir} instead.
      *
+     * This is provided for code that, for some reason, needs to bypass the ( userDir || sharedDir )
+     * fallback behavior and access the shared dir specifically.
      */
-    public static final String glimpseDncFlatDirProp = "glimpse.dnc.flatDir";
+    public static final File glimpseDncDefaultSharedFlatDir = new File( glimpseSharedDataDir, "DNC_FLAT" );
 
     /**
-     *
+     * Standard dir for DNC_FLAT permanent data.
+     * <ol>
+     * <li>JVM prop (if defined): {@code glimpse.dnc.flatDir}
+     * <li>Env var (if defined): {@code GLIMPSE_DNC_FLAT}
+     * <li>User default (if dir exists): subdir DNC_FLAT of {@link GlimpseDataPaths#glimpseUserDataDir}
+     * <li>Shared default: subdir DNC_FLAT of {@link GlimpseDataPaths#glimpseSharedDataDir}
+     * </ol>
      */
-    public static final File glimpseDncFlatDir;
-    static
+    public static final File glimpseDncFlatDir = glimpseDncFlatDir( );
+    private static File glimpseDncFlatDir( )
     {
-        String override = System.getProperty( glimpseDncFlatDirProp );
-        if ( override != null )
+        String jvmProp = System.getProperty( "glimpse.dnc.flatDir" );
+        if ( jvmProp != null )
         {
-            glimpseDncFlatDir = new File( override );
+            return new File( jvmProp );
         }
-        else
+
+        String envVar = System.getenv( "GLIMPSE_DNC_FLAT" );
+        if ( envVar != null )
         {
-            glimpseDncFlatDir = ( glimpseDncDefaultUserFlatDir.isDirectory( ) ? glimpseDncDefaultUserFlatDir : glimpseDncDefaultSharedFlatDir );
+            return new File( envVar );
         }
+
+        return ( glimpseDncDefaultUserFlatDir.isDirectory( ) ? glimpseDncDefaultUserFlatDir : glimpseDncDefaultSharedFlatDir );
     }
 
     /**
-     *
+     * Standard dir for DNC_RENDER cache data.
+     * <ol>
+     * <li>JVM prop (if defined): {@code glimpse.dnc.renderDir}
+     * <li>Env var (if defined): {@code GLIMPSE_DNC_RENDER}
+     * <li>Default: subdir DNC_RENDER of {@link GlimpseDataPaths#glimpseUserCacheDir}
+     * </ol>
      */
-    public static final String glimpseDncRenderDirProp = "glimpse.dnc.renderDir";
-
-    /**
-     *
-     */
-    public static final File glimpseDncRenderDir;
-    static
+    public static final File glimpseDncRenderDir = glimpseDncRenderDir( );
+    private static File glimpseDncRenderDir( )
     {
-        String override = System.getProperty( glimpseDncRenderDirProp );
-        if ( override != null )
+        String jvmProp = System.getProperty( "glimpse.dnc.renderDir" );
+        if ( jvmProp != null )
         {
-            glimpseDncRenderDir = new File( override );
+            return new File( jvmProp );
         }
-        else
+
+        String envVar = System.getenv( "GLIMPSE_DNC_RENDER" );
+        if ( envVar != null )
         {
-            glimpseDncRenderDir = new File( glimpseUserCacheDir, "DNC_RENDER" );
+            return new File( envVar );
         }
+
+        return new File( glimpseUserCacheDir, "DNC_RENDER" );
     }
 
     /**
-     *
+     * Standard dir for DNC_QUERY cache data.
+     * <ol>
+     * <li>JVM prop (if defined): {@code glimpse.dnc.queryDir}
+     * <li>Env var (if defined): {@code GLIMPSE_DNC_QUERY}
+     * <li>Default: subdir DNC_QUERY of {@link GlimpseDataPaths#glimpseUserCacheDir}
+     * </ol>
      */
-    public static final String glimpseDncQueryDirProp = "glimpse.dnc.queryDir";
+    public static final File glimpseDncQueryDir = glimpseDncQueryDir( );
+    private static File glimpseDncQueryDir( )
+    {
+        String jvmProp = System.getProperty( "glimpse.dnc.queryDir" );
+        if ( jvmProp != null )
+        {
+            return new File( jvmProp );
+        }
 
-    /**
-     *
-     */
-    public static final File glimpseDncQueryDir;
+        String envVar = System.getenv( "GLIMPSE_DNC_QUERY" );
+        if ( envVar != null )
+        {
+            return new File( envVar );
+        }
+
+        return new File( glimpseUserCacheDir, "DNC_QUERY" );
+    }
+
+    // Log dir locations, after they've been initialized
+    private static final Logger logger = getLogger( DncDataPaths.class );
     static
     {
-        String override = System.getProperty( glimpseDncQueryDirProp );
-        if ( override != null )
-        {
-            glimpseDncQueryDir = new File( override );
-        }
-        else
-        {
-            glimpseDncQueryDir = new File( glimpseUserCacheDir, "DNC_QUERY" );
-        }
+        logInfo( logger, "Default DNC_FLAT user dir: %s", glimpseDncDefaultUserFlatDir );
+        logInfo( logger, "Default DNC_FLAT shared dir: %s", glimpseDncDefaultSharedFlatDir );
+        logInfo( logger, "Standard DNC_FLAT dir: %s", glimpseDncFlatDir );
+        logInfo( logger, "Standard DNC_RENDER dir: %s", glimpseDncRenderDir );
+        logInfo( logger, "Standard DNC_QUERY dir: %s", glimpseDncQueryDir );
     }
 
 }
