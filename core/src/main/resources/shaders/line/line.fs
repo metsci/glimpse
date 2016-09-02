ab21d47f327ea094b431c2dbbe8c5c39971a6b8b
Mike Hogye <hogye@metsci.com>
2016-09-02 09:52:11 -0400

    Fix buggy stippling

--- core/src/main/resources/shaders/line/line.fs
+++ core/src/main/resources/shaders/line/line.fs
@@ -15,73 +15,67 @@ out vec4 outRgba;
 
 void main( )
 {
-    // The word "excursion" is used here to mean the distance beyond the edge of the fully
-    // filled region. Along each edge, there is a narrow feather region. Half the thickness
-    // of the feather region lies inside the quad, and half outside. When excursion <= 0, no
-    // extra fading occurs. When excursion >= FEATHER_THICKNESS_PX, the fragment is completely
-    // faded, and has an alpha of zero.
+    float feather_PX = max( 0.01, FEATHER_THICKNESS_PX );
+    float halfFeather_PX = 0.5 * feather_PX;
 
-    float halfFeather_PX = 0.5 * FEATHER_THICKNESS_PX;
-
-    float normalExcursion_PX;
+    float normalAlpha;
     {
         float edge_PX = 0.5*LINE_THICKNESS_PX - halfFeather_PX;
-        normalExcursion_PX = abs( gPosInQuad_PX.t ) - edge_PX;
+        float excursion_PX = abs( gPosInQuad_PX.t ) - edge_PX;
+        normalAlpha = 1.0 - ( excursion_PX / feather_PX );
     }
 
-    float startExcursion_PX;
+    float startAlpha;
     {
         float edge_PX = halfFeather_PX;
-        startExcursion_PX = edge_PX - gPosInQuad_PX.s;
+        float excursion_PX = edge_PX - gPosInQuad_PX.s;
+        startAlpha = 1.0 - ( excursion_PX / feather_PX );
     }
 
-    float endExcursion_PX;
+    float endAlpha;
     {
         float edge_PX = gQuadLength_PX - halfFeather_PX;
-        endExcursion_PX = gPosInQuad_PX.s - edge_PX;
+        float excursion_PX = gPosInQuad_PX.s - edge_PX;
+        endAlpha = 1.0 - ( excursion_PX / feather_PX );
     }
 
-    float stippleExcursion_PX;
+    float stippleAlpha;
     if ( STIPPLE_ENABLE == 0 )
     {
-        stippleExcursion_PX = 0.0;
+        stippleAlpha = 1.0;
     }
     else
     {
-        // This assumes that the feather region is thinner than a single stipple-bit region.
-        // The alternative would be complicated, and only very rarely useful.
+        // This assumes that the feather region is thinner than a single stipple-
+        // bit region. The alternative would be more complicated than useful.
 
-        float bitNum = mod( gCumulativeDistance_PX / STIPPLE_SCALE, 16.0 );
-        int bitNumPrev = int( bitNum );
-        int bitNumNext = int( mod( bitNum + 1.0, 16.0 ) );
-        int bitPrev = ( STIPPLE_PATTERN & ( 0x1 << bitNumPrev ) );
-        int bitNext = ( STIPPLE_PATTERN & ( 0x1 << bitNumNext ) );
+        float bitWidth_PX = STIPPLE_SCALE;
+        float bitNum = mod( gCumulativeDistance_PX / bitWidth_PX, 16.0 );
+        float bitAlpha = float( sign( STIPPLE_PATTERN & ( 0x1 << int( bitNum ) ) ) );
 
-        if ( bitPrev == 0 && bitNext == 0 )
+        float posInBit_PX = bitWidth_PX*( bitNum - floor( bitNum ) );
+        if ( posInBit_PX < halfFeather_PX )
         {
-            // Off
-            stippleExcursion_PX = FEATHER_THICKNESS_PX;
+            float bitNumPrev = mod( bitNum - 1.0, 16.0 );
+            float bitAlphaPrev = float( sign( STIPPLE_PATTERN & ( 0x1 << int( bitNumPrev ) ) ) );
+            float mixFrac = ( halfFeather_PX + posInBit_PX ) / feather_PX;
+            stippleAlpha = mix( bitAlphaPrev, bitAlpha, mixFrac );
         }
-        else if ( bitPrev == 0 )
+        else if ( posInBit_PX > bitWidth_PX - halfFeather_PX )
         {
-            // Rising edge
-            stippleExcursion_PX = STIPPLE_SCALE * mod( bitNumNext - bitNum, 16.0 );
-        }
-        else if ( bitNext == 0 )
-        {
-            // Falling edge
-            stippleExcursion_PX = STIPPLE_SCALE * ( bitNum - bitNumPrev );
+            float bitNumNext = mod( bitNum + 1.0, 16.0 );
+            float bitAlphaNext = float( sign( STIPPLE_PATTERN & ( 0x1 << int( bitNumNext ) ) ) );
+            float mixFrac = ( posInBit_PX - ( bitWidth_PX - halfFeather_PX ) ) / feather_PX;
+            stippleAlpha = mix( bitAlpha, bitAlphaNext, mixFrac );
         }
         else
         {
-            // On
-            stippleExcursion_PX = 0.0;
+            stippleAlpha = bitAlpha;
         }
     }
 
     outRgba.rgb = RGBA.rgb;
 
-    float maxExcursion_PX = max( max( startExcursion_PX, endExcursion_PX ), max( normalExcursion_PX, stippleExcursion_PX ) );
-    float fade = clamp( maxExcursion_PX / FEATHER_THICKNESS_PX, 0.0, 1.0 );
-    outRgba.a = RGBA.a * ( 1.0 - fade );
+    float minAlpha = min( min( startAlpha, endAlpha ), min( normalAlpha, stippleAlpha ) );
+    outRgba.a = RGBA.a * clamp( minAlpha, 0.0, 1.0 );
 }
