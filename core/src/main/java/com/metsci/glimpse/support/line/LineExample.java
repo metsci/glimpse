35f986a863d80902b855e55cb80ed37ada1251b4
Mike Hogye <hogye@metsci.com>
2016-09-03 14:54:04 -0400

    Continue cleaning up line code for reusability

--- core/src/main/java/com/metsci/glimpse/support/line/LineExample.java
+++ core/src/main/java/com/metsci/glimpse/support/line/LineExample.java
@@ -1,23 +1,17 @@
 package com.metsci.glimpse.support.line;
 
-import static com.jogamp.common.nio.Buffers.SIZEOF_FLOAT;
 import static com.metsci.glimpse.gl.util.GLUtils.genBuffer;
 import static com.metsci.glimpse.support.FrameUtils.disposeOnWindowClosing;
 import static com.metsci.glimpse.support.FrameUtils.newFrame;
 import static com.metsci.glimpse.support.FrameUtils.showFrameCentered;
 import static com.metsci.glimpse.support.FrameUtils.stopOnWindowClosing;
 import static com.metsci.glimpse.support.line.LineUtils.distance;
+import static com.metsci.glimpse.support.line.LineUtils.enableStandardBlending;
+import static com.metsci.glimpse.support.line.LineUtils.orphanAndMapFloats;
 import static com.metsci.glimpse.support.line.LineUtils.put1f;
 import static com.metsci.glimpse.support.line.LineUtils.put2f;
 import static com.metsci.glimpse.util.GeneralUtils.floats;
-import static java.lang.System.currentTimeMillis;
 import static javax.media.opengl.GL.GL_ARRAY_BUFFER;
-import static javax.media.opengl.GL.GL_BLEND;
-import static javax.media.opengl.GL.GL_MAP_UNSYNCHRONIZED_BIT;
-import static javax.media.opengl.GL.GL_MAP_WRITE_BIT;
-import static javax.media.opengl.GL.GL_ONE;
-import static javax.media.opengl.GL.GL_ONE_MINUS_SRC_ALPHA;
-import static javax.media.opengl.GL.GL_SRC_ALPHA;
 import static javax.media.opengl.GL2ES2.GL_STREAM_DRAW;
 import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
 
@@ -61,10 +55,11 @@ public class LineExample
 
             int xyVbo = 0;
             int cumulativeDistanceVbo = 0;
+            int numVertices = 0;
 
             {
                 style.rgba = floats( 0.7f, 0, 0, 1 );
-                style.thickness_PX = 4;
+                style.thickness_PX = 1;
                 style.stippleEnable = true;
                 style.stippleScale = 2;
                 style.stipplePattern = 0b0001010111111111;
@@ -75,53 +70,25 @@ public class LineExample
                 GL2ES2 gl = context.getGL( ).getGL2ES2( );
 
 
-                if ( prog == null )
+                // Init
+
+                if ( prog == null || xyVbo == 0 || cumulativeDistanceVbo == 0 )
                 {
-                    prog = new LineProgram( gl );
-                }
-
-                if ( xyVbo == 0 )
-                {
-                    xyVbo = genBuffer( gl );
-                }
-
-                if ( cumulativeDistanceVbo == 0 )
-                {
-                    cumulativeDistanceVbo = genBuffer( gl );
-                }
+                    this.prog = new LineProgram( gl );
+                    this.xyVbo = genBuffer( gl );
+                    this.cumulativeDistanceVbo = genBuffer( gl );
 
 
+                    int maxVertices = 1000000;
 
-                gl.glBlendFuncSeparate( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA );
-                gl.glEnable( GL_BLEND );
-
-
-                prog.begin( gl );
-                try
-                {
-                    prog.setViewport( gl, bounds );
-                    prog.setAxisOrtho( gl, axis );
-                    prog.setStyle( gl, style );
-
-
-
-
-                    int maxVertices = 1024;
-
-
-                    int xyMaxBytes = maxVertices * 2 * SIZEOF_FLOAT;
                     gl.glBindBuffer( GL_ARRAY_BUFFER, xyVbo );
-                    gl.glBufferData( GL_ARRAY_BUFFER, xyMaxBytes, null, GL_STREAM_DRAW );
-                    FloatBuffer xyBuffer = gl.glMapBufferRange( GL_ARRAY_BUFFER, 0, xyMaxBytes, GL_MAP_WRITE_BIT | GL_MAP_UNSYNCHRONIZED_BIT ).asFloatBuffer( );
+                    FloatBuffer xyBuffer = orphanAndMapFloats( gl, GL_ARRAY_BUFFER, 2*maxVertices, GL_STREAM_DRAW );
 
-                    int cumulativeDistanceMaxBytes = maxVertices * 1 * SIZEOF_FLOAT;
                     gl.glBindBuffer( GL_ARRAY_BUFFER, cumulativeDistanceVbo );
-                    gl.glBufferData( GL_ARRAY_BUFFER, cumulativeDistanceMaxBytes, null, GL_STREAM_DRAW );
-                    FloatBuffer cumulativeDistanceBuffer = gl.glMapBufferRange( GL_ARRAY_BUFFER, 0, cumulativeDistanceMaxBytes, GL_MAP_WRITE_BIT | GL_MAP_UNSYNCHRONIZED_BIT ).asFloatBuffer( );
+                    FloatBuffer cumulativeDistanceBuffer = orphanAndMapFloats( gl, GL_ARRAY_BUFFER, 1*maxVertices, GL_STREAM_DRAW );
 
-
-                    Random r = new Random( currentTimeMillis( ) / 750 );
-                    for ( int i = 0; i < 20; i++ )
+                    Random r = new Random( 0 );
+                    for ( int i = 0; i < 250000; i++ )
                     {
                         double x0 = 2 + 6*r.nextDouble( );
                         double y0 = 2 + 6*r.nextDouble( );
@@ -143,15 +110,26 @@ public class LineExample
                         put1f( cumulativeDistanceBuffer, distance( x0, y0, x1, y1 ) + distance( x1, y1, x2, y2 ) );
                     }
 
-
-                    int numVertices = xyBuffer.position( ) / 2;
+                    this.numVertices = xyBuffer.position( ) / 2;
 
                     gl.glBindBuffer( GL_ARRAY_BUFFER, xyVbo );
                     gl.glUnmapBuffer( GL_ARRAY_BUFFER );
 
                     gl.glBindBuffer( GL_ARRAY_BUFFER, cumulativeDistanceVbo );
                     gl.glUnmapBuffer( GL_ARRAY_BUFFER );
+                }
 
+
+                // Render
+
+                enableStandardBlending( gl );
+
+                prog.begin( gl );
+                try
+                {
+                    prog.setViewport( gl, bounds );
+                    prog.setAxisOrtho( gl, axis );
+                    prog.setStyle( gl, style );
 
                     prog.draw( gl, xyVbo, cumulativeDistanceVbo, 0, numVertices );
                 }
@@ -182,26 +160,66 @@ public class LineExample
                 GL2ES2 gl = context.getGL( ).getGL2ES2( );
 
 
-                if ( prog == null )
-                {
-                    prog = new LineProgram( gl );
-                }
+                // Init
 
-                if ( xyVbo == 0 )
+                if ( prog == null || xyVbo == 0 || cumulativeDistanceVbo == 0 )
                 {
-                    xyVbo = genBuffer( gl );
-                }
-
-                if ( cumulativeDistanceVbo == 0 )
-                {
-                    cumulativeDistanceVbo = genBuffer( gl );
+                    this.prog = new LineProgram( gl );
+                    this.xyVbo = genBuffer( gl );
+                    this.cumulativeDistanceVbo = genBuffer( gl );
                 }
 
 
+                // Update
 
-                gl.glBlendFuncSeparate( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA );
-                gl.glEnable( GL_BLEND );
+                int maxVertices = 8;
 
+                gl.glBindBuffer( GL_ARRAY_BUFFER, xyVbo );
+                FloatBuffer xyBuffer = orphanAndMapFloats( gl, GL_ARRAY_BUFFER, 2*maxVertices, GL_STREAM_DRAW );
+
+                gl.glBindBuffer( GL_ARRAY_BUFFER, cumulativeDistanceVbo );
+                FloatBuffer cumulativeDistanceBuffer = orphanAndMapFloats( gl, GL_ARRAY_BUFFER, 1*maxVertices, GL_STREAM_DRAW );
+
+
+                float inset_PX = 0.5f * style.thickness_PX;
+                float xLeft_PX = inset_PX;
+                float xRight_PX = bounds.getWidth( ) - inset_PX;
+                float yBottom_PX = inset_PX;
+                float yTop_PX = bounds.getHeight( ) - inset_PX;
+
+                xyBuffer.put( xLeft_PX  ).put( yBottom_PX );
+                xyBuffer.put( xRight_PX ).put( yBottom_PX );
+                cumulativeDistanceBuffer.put( 0 );
+                cumulativeDistanceBuffer.put( xRight_PX - xLeft_PX );
+
+                xyBuffer.put( xRight_PX ).put( yBottom_PX );
+                xyBuffer.put( xRight_PX ).put( yTop_PX    );
+                cumulativeDistanceBuffer.put( 0 );
+                cumulativeDistanceBuffer.put( yTop_PX - yBottom_PX );
+
+                xyBuffer.put( xLeft_PX ).put( yBottom_PX );
+                xyBuffer.put( xLeft_PX ).put( yTop_PX    );
+                cumulativeDistanceBuffer.put( 0 );
+                cumulativeDistanceBuffer.put( yTop_PX - yBottom_PX );
+
+                xyBuffer.put( xLeft_PX  ).put( yTop_PX );
+                xyBuffer.put( xRight_PX ).put( yTop_PX );
+                cumulativeDistanceBuffer.put( 0 );
+                cumulativeDistanceBuffer.put( xRight_PX - xLeft_PX );
+
+
+                int numVertices = xyBuffer.position( ) / 2;
+
+                gl.glBindBuffer( GL_ARRAY_BUFFER, xyVbo );
+                gl.glUnmapBuffer( GL_ARRAY_BUFFER );
+
+                gl.glBindBuffer( GL_ARRAY_BUFFER, cumulativeDistanceVbo );
+                gl.glUnmapBuffer( GL_ARRAY_BUFFER );
+
+
+                // Render
+
+                enableStandardBlending( gl );
 
                 prog.begin( gl );
                 try
@@ -209,58 +227,6 @@ public class LineExample
                     prog.setViewport( gl, bounds );
                     prog.setPixelOrtho( gl, bounds );
                     prog.setStyle( gl, style );
-
-                    float inset_PX = 0.5f * style.thickness_PX;
-                    float xLeft_PX = inset_PX;
-                    float xRight_PX = bounds.getWidth( ) - inset_PX;
-                    float yBottom_PX = inset_PX;
-                    float yTop_PX = bounds.getHeight( ) - inset_PX;
-
-
-
-                    int maxVertices = 100;
-
-
-                    int xyMaxBytes = maxVertices * 2 * SIZEOF_FLOAT;
-                    gl.glBindBuffer( GL_ARRAY_BUFFER, xyVbo );
-                    gl.glBufferData( GL_ARRAY_BUFFER, xyMaxBytes, null, GL_STREAM_DRAW );
-                    FloatBuffer xyBuffer = gl.glMapBufferRange( GL_ARRAY_BUFFER, 0, xyMaxBytes, GL_MAP_WRITE_BIT | GL_MAP_UNSYNCHRONIZED_BIT ).asFloatBuffer( );
-
-                    int cumulativeDistanceMaxBytes = maxVertices * 1 * SIZEOF_FLOAT;
-                    gl.glBindBuffer( GL_ARRAY_BUFFER, cumulativeDistanceVbo );
-                    gl.glBufferData( GL_ARRAY_BUFFER, cumulativeDistanceMaxBytes, null, GL_STREAM_DRAW );
-                    FloatBuffer cumulativeDistanceBuffer = gl.glMapBufferRange( GL_ARRAY_BUFFER, 0, cumulativeDistanceMaxBytes, GL_MAP_WRITE_BIT | GL_MAP_UNSYNCHRONIZED_BIT ).asFloatBuffer( );
-
-
-                    xyBuffer.put( xLeft_PX  ).put( yBottom_PX );
-                    xyBuffer.put( xRight_PX ).put( yBottom_PX );
-                    cumulativeDistanceBuffer.put( 0 );
-                    cumulativeDistanceBuffer.put( xRight_PX - xLeft_PX );
-
-                    xyBuffer.put( xRight_PX ).put( yBottom_PX );
-                    xyBuffer.put( xRight_PX ).put( yTop_PX    );
-                    cumulativeDistanceBuffer.put( 0 );
-                    cumulativeDistanceBuffer.put( yTop_PX - yBottom_PX );
-
-                    xyBuffer.put( xLeft_PX ).put( yBottom_PX );
-                    xyBuffer.put( xLeft_PX ).put( yTop_PX    );
-                    cumulativeDistanceBuffer.put( 0 );
-                    cumulativeDistanceBuffer.put( yTop_PX - yBottom_PX );
-
-                    xyBuffer.put( xLeft_PX  ).put( yTop_PX );
-                    xyBuffer.put( xRight_PX ).put( yTop_PX );
-                    cumulativeDistanceBuffer.put( 0 );
-                    cumulativeDistanceBuffer.put( xRight_PX - xLeft_PX );
-
-
-                    int numVertices = xyBuffer.position( ) / 2;
-
-                    gl.glBindBuffer( GL_ARRAY_BUFFER, xyVbo );
-                    gl.glUnmapBuffer( GL_ARRAY_BUFFER );
-
-                    gl.glBindBuffer( GL_ARRAY_BUFFER, cumulativeDistanceVbo );
-                    gl.glUnmapBuffer( GL_ARRAY_BUFFER );
-
 
                     prog.draw( gl, xyVbo, cumulativeDistanceVbo, 0, numVertices );
                 }
--- core/src/main/java/com/metsci/glimpse/support/line/LineUtils.java
+++ core/src/main/java/com/metsci/glimpse/support/line/LineUtils.java
@@ -1,11 +1,29 @@
 package com.metsci.glimpse.support.line;
 
+import static com.jogamp.common.nio.Buffers.SIZEOF_FLOAT;
 import static java.lang.Math.sqrt;
+import static javax.media.opengl.GL.GL_ARRAY_BUFFER;
+import static javax.media.opengl.GL.GL_BLEND;
+import static javax.media.opengl.GL.GL_MAP_UNSYNCHRONIZED_BIT;
+import static javax.media.opengl.GL.GL_MAP_WRITE_BIT;
+import static javax.media.opengl.GL.GL_ONE;
+import static javax.media.opengl.GL.GL_ONE_MINUS_SRC_ALPHA;
+import static javax.media.opengl.GL.GL_SRC_ALPHA;
+import static javax.media.opengl.GL2ES2.GL_STREAM_DRAW;
 
+import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
+
+import javax.media.opengl.GL;
 
 public class LineUtils
 {
+
+    public static void enableStandardBlending( GL gl )
+    {
+        gl.glBlendFuncSeparate( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ONE_MINUS_SRC_ALPHA );
+        gl.glEnable( GL_BLEND );
+    }
 
     public static double distance( double x0, double y0, double x1, double y1 )
     {
@@ -23,6 +41,17 @@ public class LineUtils
     {
         buffer.put( ( float ) a )
               .put( ( float ) b );
+    }
+
+    public static FloatBuffer orphanAndMapFloats( GL gl, int target, long numFloats, int usage )
+    {
+        return orphanAndMapBytes( gl, target, numFloats * SIZEOF_FLOAT, usage ).asFloatBuffer( );
+    }
+
+    public static ByteBuffer orphanAndMapBytes( GL gl, int target, long numBytes, int usage )
+    {
+        gl.glBufferData( GL_ARRAY_BUFFER, numBytes, null, GL_STREAM_DRAW );
+        return gl.glMapBufferRange( GL_ARRAY_BUFFER, 0, numBytes, GL_MAP_WRITE_BIT | GL_MAP_UNSYNCHRONIZED_BIT );
     }
 
 }
