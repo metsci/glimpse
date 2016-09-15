a2c9ff75a1a49b34a4b0d952e263c07537b0ef94
Geoffrey Ulman <ulman@metsci.com>
2016-09-15 13:31:07 -0400

    Refactor TextExample for GlimpsePainter changes

--- core-examples/src/main/java/com/metsci/glimpse/examples/icon/TextExample.java
+++ core-examples/src/main/java/com/metsci/glimpse/examples/icon/TextExample.java
@@ -3,7 +3,8 @@ package com.metsci.glimpse.examples.icon;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-import javax.media.opengl.GL2;
+import javax.media.opengl.GL;
+import javax.media.opengl.GL3;
 import javax.media.opengl.fixedfunc.GLMatrixFunc;
 
 import com.jogamp.graph.curve.Region;
@@ -16,17 +17,19 @@ import com.jogamp.graph.geom.SVertex;
 import com.jogamp.opengl.util.PMVMatrix;
 import com.metsci.glimpse.axis.Axis2D;
 import com.metsci.glimpse.context.GlimpseBounds;
+import com.metsci.glimpse.context.GlimpseContext;
 import com.metsci.glimpse.examples.Example;
+import com.metsci.glimpse.gl.util.GLUtils;
 import com.metsci.glimpse.layout.GlimpseLayout;
 import com.metsci.glimpse.layout.GlimpseLayoutProvider;
-import com.metsci.glimpse.painter.base.GlimpseDataPainter2D;
 import com.metsci.glimpse.painter.base.GlimpsePainter;
-import com.metsci.glimpse.plot.SimplePlot2D;
+import com.metsci.glimpse.painter.base.GlimpsePainterBase;
+import com.metsci.glimpse.plot.EmptyPlot2D;
 import com.metsci.glimpse.support.color.GlimpseColor;
 
 public class TextExample implements GlimpseLayoutProvider
 {
-    private static final Logger logger = Logger.getLogger( IconPainterExample.class.getName( ) );
+    private static final Logger logger = Logger.getLogger( TextExample.class.getName( ) );
 
     public static void main( String[] args ) throws Exception
     {
@@ -36,7 +39,7 @@ public class TextExample implements GlimpseLayoutProvider
     @Override
     public GlimpseLayout getLayout( ) throws Exception
     {
-        SimplePlot2D plot = new SimplePlot2D( );
+        EmptyPlot2D plot = new EmptyPlot2D( );
 
         Font loadFont = null;
         //InputStream fontFile = StreamOpener.fileThenResource.openForRead( "fonts/bitstream/Vera.ttf" );
@@ -58,52 +61,71 @@ public class TextExample implements GlimpseLayoutProvider
         final TextRegionUtil util = new TextRegionUtil( Region.DEFAULT_TWO_PASS_TEXTURE_UNIT | Region.COLORCHANNEL_RENDERING_BIT );
         final Font font = loadFont;
 
-        GlimpsePainter painter = new GlimpseDataPainter2D( )
+        GlimpsePainter painter = new GlimpsePainterBase( )
         {
             protected boolean initialized = false;
 
             @Override
-            public void paintTo( GL2 gl, GlimpseBounds bounds, Axis2D axis )
+            public void doPaintTo( GlimpseContext context )
             {
+                Axis2D axis = getAxis2D( context );
+                GlimpseBounds bounds = getBounds( context );
+                GL3 gl = context.getGL( ).getGL3( );
+
                 if ( !initialized )
                 {
                     renderer.init( gl, Region.DEFAULT_TWO_PASS_TEXTURE_UNIT | Region.COLORCHANNEL_RENDERING_BIT );
                     initialized = true;
                 }
 
-                PMVMatrix m = renderer.getMatrix( );
-                m.glMatrixMode( GLMatrixFunc.GL_MODELVIEW );
-                m.glLoadIdentity( );
-                m.glTranslatef( 5, 7, 0 );
+                GLUtils.enableStandardBlending( gl );
+                gl.glEnable( GL.GL_CULL_FACE );
+                try
+                {
+                    PMVMatrix m = renderer.getMatrix( );
+                    m.glMatrixMode( GLMatrixFunc.GL_MODELVIEW );
+                    m.glLoadIdentity( );
+                    m.glTranslatef( 5, 7, 0 );
 
-                m.glMatrixMode( GLMatrixFunc.GL_PROJECTION );
-                m.glLoadIdentity( );
-                m.glOrthof( ( float ) axis.getMinX( ), ( float ) axis.getMaxX( ), ( float ) axis.getMinY( ), ( float ) axis.getMaxY( ), -1f, 1f );
+                    m.glMatrixMode( GLMatrixFunc.GL_PROJECTION );
+                    m.glLoadIdentity( );
+                    m.glOrthof( ( float ) axis.getMinX( ), ( float ) axis.getMaxX( ), ( float ) axis.getMinY( ), ( float ) axis.getMaxY( ), -1f, 1f );
 
-                float pixelSize = 1.0f; //font.getPixelSize( 32, 96 );
-                int[] samples = new int[1];
-                //String text = "Test";
-                String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
-                util.drawString3D( gl, renderer, font, pixelSize, text, GlimpseColor.getRed( ), samples );
-                
-                
-                
-                m.glMatrixMode( GLMatrixFunc.GL_PROJECTION );
-                m.glLoadIdentity( );
-                m.glOrthof( 0, bounds.getWidth( ), 0, bounds.getHeight( ), -1f, 1f );
+                    float pixelSize = 1.0f; //font.getPixelSize( 32, 96 );
+                    int[] samples = new int[1];
+                    //String text = "Test";
+                    String text = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+                    util.drawString3D( gl, renderer, font, pixelSize, text, GlimpseColor.getRed( ), samples );
 
-                m.glMatrixMode( GLMatrixFunc.GL_MODELVIEW );
-                m.glLoadIdentity( );
-                m.glTranslatef( (float) axis.getAxisX( ).valueToScreenPixelUnits( 5 ), (float) axis.getAxisY( ).valueToScreenPixelUnits( 5 ), 0f );
-                
-                pixelSize = font.getPixelSize( 32, 96 );
-                util.drawString3D( gl, renderer, font, pixelSize, text, GlimpseColor.getRed( ), samples );
-                
-                renderer.enable( gl, false );
+                    m.glMatrixMode( GLMatrixFunc.GL_PROJECTION );
+                    m.glLoadIdentity( );
+                    m.glOrthof( 0, bounds.getWidth( ), 0, bounds.getHeight( ), -1f, 1f );
+
+                    m.glMatrixMode( GLMatrixFunc.GL_MODELVIEW );
+                    m.glLoadIdentity( );
+                    m.glTranslatef( ( float ) axis.getAxisX( ).valueToScreenPixelUnits( 5 ), ( float ) axis.getAxisY( ).valueToScreenPixelUnits( 5 ), 0f );
+
+                    pixelSize = font.getPixelSize( 32, 96 );
+                    util.drawString3D( gl, renderer, font, pixelSize, text, GlimpseColor.getRed( ), samples );
+
+                    renderer.enable( gl, false );
+                }
+                finally
+                {
+                    gl.glDisable( GL.GL_CULL_FACE );
+                    GLUtils.disableBlending( gl );
+                }
+            }
+
+            @Override
+            protected void doDispose( GlimpseContext context )
+            {
+                // TODO Auto-generated method stub
+
             }
         };
 
-        plot.getLayoutCenter( ).addPainter( painter );
+        plot.addPainter( painter );
 
         return plot;
     }
