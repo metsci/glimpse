493dac8df9bef21b4e4ab17df96d77131d7ad3b8
Mike Hogye <hogye@metsci.com>
2016-10-07 08:49:42 -0400

    Clean up LineStripExample

--- core-examples/src/main/java/com/metsci/glimpse/examples/basic/LineStripExample.java
+++ core-examples/src/main/java/com/metsci/glimpse/examples/basic/LineStripExample.java
@@ -5,14 +5,11 @@ import static com.metsci.glimpse.support.FrameUtils.*;
 import static com.metsci.glimpse.support.shader.line.LineJoinType.*;
 import static com.metsci.glimpse.support.shader.line.LineUtils.*;
 import static com.metsci.glimpse.util.GeneralUtils.*;
-import static java.nio.FloatBuffer.*;
-import static java.util.concurrent.Executors.*;
-import static java.util.concurrent.TimeUnit.*;
+import static java.lang.System.*;
 import static javax.media.opengl.GLProfile.*;
 import static javax.swing.WindowConstants.*;
 
 import java.util.Random;
-import java.util.concurrent.ScheduledExecutorService;
 
 import javax.media.opengl.GL2ES2;
 import javax.media.opengl.GL2ES3;
@@ -20,7 +17,6 @@ import javax.media.opengl.GLAnimatorControl;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.metsci.glimpse.axis.Axis2D;
 import com.metsci.glimpse.context.GlimpseBounds;
 import com.metsci.glimpse.context.GlimpseContext;
@@ -69,45 +65,21 @@ public class LineStripExample
         protected LineStyle style;
         protected LineProgram prog;
 
+        protected long recentChange_PMILLIS = 0;
+        protected final Random random = new Random( 0 );
+
         public LineStripExamplePainter( )
         {
             this.strip = new LineStrip( 64 );
-
-
-            final Random r = new Random( 0 );
-            Runnable addVertex = new Runnable( )
-            {
-                float x = 0;
-                float y = 0;
-
-                public void run( )
-                {
-                    SwingUtilities.invokeLater( new Runnable( )
-                    {
-                        public void run( )
-                        {
-                            strip.put( 0, wrap( floats( r.nextFloat( ) - 0.5f, r.nextFloat( ) - 0.5f ) ) );
-
-                            strip.grow( 2 );
-                            strip.put( wrap( floats( x, y ) ) );
-                            x += r.nextFloat( );
-                            y += 3 * ( r.nextFloat( ) - 0.5f );
-                        }
-                    } );
-                }
-            };
-            ScheduledExecutorService scheduler = newSingleThreadScheduledExecutor( new ThreadFactoryBuilder( ).setDaemon( true ).build( ) );
-            scheduler.scheduleWithFixedDelay( addVertex, 250, 250, MILLISECONDS );
-
 
             // Set line appearance (except for thickness, which is set in doPaintTo)
             this.style = new LineStyle( );
             style.thickness_PX = 4;
             style.joinType = JOIN_MITER;
             style.rgba = floats( 0.7f, 0, 0, 1 );
-            style.stippleEnable = true;
-            style.stippleScale = 2;
-            style.stipplePattern = 0b0001010111111111;
+            //style.stippleEnable = true;
+            //style.stippleScale = 2;
+            //style.stipplePattern = 0b0001010111111111;
 
             // Create the shader program for drawing lines
             this.prog = new LineProgram( );
@@ -116,6 +88,25 @@ public class LineStripExample
         @Override
         public void doPaintTo( GlimpseContext context )
         {
+            // Change the line strip periodically
+            long time_PMILLIS = currentTimeMillis( );
+            if ( time_PMILLIS - this.recentChange_PMILLIS > 100 )
+            {
+                // Append a vertex
+                float x = strip.logicalSize( );
+                float y = x + 10*( random.nextFloat( ) - 0.5f );
+                strip.grow( 2 );
+                strip.put( x, y );
+
+                // Move an existing vertex
+                int iPerturb = strip.logicalSize( ) / 4;
+                float xPerturb = iPerturb;
+                float yPerturb = xPerturb + 10*( random.nextFloat( ) - 0.5f );
+                strip.put( iPerturb, xPerturb, yPerturb );
+
+                this.recentChange_PMILLIS = time_PMILLIS;
+            }
+
             GlimpseBounds bounds = getBounds( context );
             Axis2D axis = requireAxis2D( context );
             GL2ES3 gl = context.getGL( ).getGL2ES3( );
