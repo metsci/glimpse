3205f272deb046c76983316399ec25d0e0eb9b5c
Mike Hogye <hogye@metsci.com>
2016-12-10 09:21:03 -0500

    Add ImmutableCollectionUtils

---
+++ util/src/main/java/com/metsci/glimpse/util/ImmutableCollectionUtils.java
@@ -0,0 +1,138 @@
+package com.metsci.glimpse.util;
+
+import static com.google.common.base.Objects.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+public class ImmutableCollectionUtils
+{
+
+    public static <K1,K2,V> ImmutableMap<K1,ImmutableMap<K2,V>> mapWith( ImmutableMap<K1,ImmutableMap<K2,V>> map, K1 key1, K2 key2, V value )
+    {
+        ImmutableMap<K2,V> innerMap = mapWith( map.get( key1 ), key2, value );
+        return mapWith( map, key1, innerMap );
+    }
+
+    public static <K,V> ImmutableMap<K,V> mapWith( ImmutableMap<K,V> map, K key, V value )
+    {
+        if ( equal( value, map.get( key ) ) )
+        {
+            return map;
+        }
+        else
+        {
+            Map<K,V> newMap = new LinkedHashMap<>( map );
+            if ( value == null )
+            {
+                newMap.remove( key );
+            }
+            else
+            {
+                newMap.put( key, value );
+            }
+            return ImmutableMap.copyOf( newMap );
+        }
+    }
+
+    public static <K,V> ImmutableMap<K,V> mapWith( ImmutableMap<K,V> map, K key, Function<? super V,? extends V> transformFn )
+    {
+        V value = transformFn.apply( map.get( key ) );
+        return mapWith( map, key, value );
+    }
+
+    public static <K,V> ImmutableMap<K,V> mapMinus( ImmutableMap<K,V> map, K key )
+    {
+        if ( !map.containsKey( key ) )
+        {
+            return map;
+        }
+        else
+        {
+            Map<K,V> newMap = new LinkedHashMap<>( map );
+            newMap.remove( key );
+            return ImmutableMap.copyOf( newMap );
+        }
+    }
+
+    public static <V> ImmutableSet<V> setPlus( ImmutableSet<V> set, V value )
+    {
+        if ( set.contains( value ) )
+        {
+            return set;
+        }
+        else
+        {
+            Set<V> newSet = new LinkedHashSet<>( set );
+            newSet.add( value );
+            return ImmutableSet.copyOf( newSet );
+        }
+    }
+
+    public static <V> ImmutableSet<V> setMinus( ImmutableSet<V> set, V value )
+    {
+        if ( !set.contains( value ) )
+        {
+            return set;
+        }
+        else
+        {
+            Set<V> newSet = new LinkedHashSet<>( set );
+            newSet.remove( value );
+            return ImmutableSet.copyOf( newSet );
+        }
+    }
+
+    public static <V> ImmutableList<V> listPlus( ImmutableList<V> list, V value )
+    {
+        List<V> newList = new ArrayList<>( list );
+        newList.add( value );
+        return ImmutableList.copyOf( newList );
+    }
+
+    public static <V> ImmutableList<V> listPlus( ImmutableList<V> list, Collection<? extends V> values )
+    {
+        List<V> newList = new ArrayList<>( list );
+        newList.addAll( values );
+        return ImmutableList.copyOf( newList );
+    }
+
+    public static <V> ImmutableList<V> listWith( ImmutableList<V> list, int index, V value )
+    {
+        if ( equal( value, list.get( index ) ) )
+        {
+            return list;
+        }
+        else
+        {
+            List<V> newList = new ArrayList<>( list );
+            newList.set( index, value );
+            return ImmutableList.copyOf( newList );
+        }
+    }
+
+    public static <V> ImmutableList<V> listMinus( ImmutableList<V> list, V value )
+    {
+        List<V> newList = new ArrayList<>( list );
+        boolean removed = newList.remove( value );
+        if ( removed )
+        {
+            return ImmutableList.copyOf( newList );
+        }
+        else
+        {
+            return list;
+        }
+    }
+
+}
