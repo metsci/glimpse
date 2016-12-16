34b7f59b38668e0f36afb4188f9189e0dd4f8148
Mike Hogye <hogye@metsci.com>
2016-12-16 09:07:21 -0500

    Add CursorLabelPainter

--- layers/src/main/java/com/metsci/glimpse/layers/misc/UiUtils.java
+++ layers/src/main/java/com/metsci/glimpse/layers/misc/UiUtils.java
@@ -1,10 +1,15 @@
 package com.metsci.glimpse.layers.misc;
 
+import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
+
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
 import java.awt.ItemSelectable;
 import java.awt.event.ItemListener;
+import java.awt.image.BufferedImage;
 
 import javax.media.opengl.GLAnimatorControl;
 import javax.media.opengl.GLAutoDrawable;
@@ -100,6 +105,96 @@ public class UiUtils
         {
             animator.remove( glDrawable );
         };
+    }
+
+    /**
+     * The component must be part of a tree that is either displayable, or whose root component's
+     * {@link Component#addNotify()} method has been called -- otherwise layout invalidation doesn't
+     * work right.
+     *
+     * For a tree that will only be painted offscreen, {@link Component#addNotify()} should be called
+     * on the root component right after it gets instantiated. In some cases it may be convenient to
+     * do this in an instance initializer:
+     *
+     *     JPanel offscreenRoot = new JPanel( )
+     *     {{
+     *         // We want to paint this component without making it displayable, so
+     *         // addNotify() must be called for layout invalidation to work right
+     *         addNotify( );
+     *     }};
+     *
+     */
+    public static BufferedImage paintComponentToImage( Component c )
+    {
+        return paintComponentToImage( c, c.getPreferredSize( ) );
+    }
+
+    /**
+     * The component must be part of a tree that is either displayable, or whose root component's
+     * {@link Component#addNotify()} method has been called -- otherwise layout invalidation doesn't
+     * work right.
+     *
+     * For a tree that will only be painted offscreen, {@link Component#addNotify()} should be called
+     * on the root component right after it gets instantiated. In some cases it may be convenient to
+     * do this in an instance initializer:
+     *
+     *     JPanel offscreenRoot = new JPanel( )
+     *     {{
+     *         // We want to paint this component without making it displayable, so
+     *         // addNotify() must be called for layout invalidation to work right
+     *         addNotify( );
+     *     }};
+     *
+     */
+    public static BufferedImage paintComponentToImage( Component c, Dimension size )
+    {
+        return paintComponentToImage( c, size.width, size.height );
+    }
+
+    /**
+     * The component must be part of a tree that is either displayable, or whose root component's
+     * {@link Component#addNotify()} method has been called -- otherwise layout invalidation doesn't
+     * work right.
+     *
+     * For a tree that will only be painted offscreen, {@link Component#addNotify()} should be called
+     * on the root component right after it gets instantiated. In some cases it may be convenient to
+     * do this in an instance initializer:
+     *
+     *     JPanel offscreenRoot = new JPanel( )
+     *     {{
+     *         // We want to paint this component without making it displayable, so
+     *         // addNotify() must be called for layout invalidation to work right
+     *         addNotify( );
+     *     }};
+     *
+     */
+    public static BufferedImage paintComponentToImage( Component c, int width, int height )
+    {
+        c.setBounds( 0, 0, width, height );
+
+        layoutTree( c );
+        c.validate( );
+
+        BufferedImage image = new BufferedImage( width, height, TYPE_INT_ARGB );
+
+        Graphics2D g = image.createGraphics( );
+        c.paint( g );
+        g.dispose( );
+
+        return image;
+    }
+
+    public static void layoutTree( Component root )
+    {
+        root.doLayout( );
+        if ( root instanceof Container )
+        {
+            Container container = ( Container ) root;
+            for ( Component child : container.getComponents( ) )
+            {
+                layoutTree( child );
+            }
+        }
     }
 
 }
