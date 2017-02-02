c00122ea53bc98adc8b17b45d75cffa1ce09a34b
Brandon Borkholder <borkholder@metsci.com>
2017-02-02 13:45:50 -0500

    Rename setCurrenTimeMarkerColor to setCurrentTimeMarkerColor

--- core/src/main/java/com/metsci/glimpse/plot/timeline/painter/SimpleSelectedTimeRegionPainter.java
+++ core/src/main/java/com/metsci/glimpse/plot/timeline/painter/SimpleSelectedTimeRegionPainter.java
@@ -102,7 +102,7 @@ public class SimpleSelectedTimeRegionPainter extends GlimpsePainterBase
         this.selectionBorderColor = color;
     }
 
-    public void setCurrenTimeMarkerColor( float[] color )
+    public void setCurrentTimeMarkerColor( float[] color )
     {
         this.currentTimeMarkerColor = color;
     }
