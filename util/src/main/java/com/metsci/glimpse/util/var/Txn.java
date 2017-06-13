05e72f3dd62348ce50d8bad39d85df90883bdac0
Mike Hogye <hogye@metsci.com>
2017-06-13 16:39:38 -0400

    Replace Var's Dispatcher/Batch mechanism with a transaction-style mechanism
    The Batch mechanism was error-prone, because it was easy to forget to pass
    a batch into a Var mutator call. It also made it difficult to write
    reusable convenience functions that called Var mutators, because each such
    convenience function needed versions with and without a Batch argument.

---
+++ util/src/main/java/com/metsci/glimpse/util/var/Txn.java
@@ -0,0 +1,54 @@
+package com.metsci.glimpse.util.var;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Txn
+{
+
+    protected static class SubtreeModification
+    {
+        public final Var<?> root;
+        public final VarEvent ev;
+
+        public SubtreeModification( Var<?> root, VarEvent ev )
+        {
+            this.root = root;
+            this.ev = ev;
+        }
+    }
+
+
+    protected final List<SubtreeModification> subtreeMods;
+
+
+    public Txn( )
+    {
+        this.subtreeMods = new ArrayList<>( );
+    }
+
+    public void recordSubtreeMod( Var<?> root, VarEvent ev )
+    {
+        this.subtreeMods.add( new SubtreeModification( root, ev ) );
+    }
+
+    public void commit( )
+    {
+        for ( SubtreeModification mod : this.subtreeMods )
+        {
+            mod.root.commitForSubtree( );
+            mod.root.fireForSubtree( mod.ev );
+        }
+        this.subtreeMods.clear( );
+    }
+
+    public void rollback( )
+    {
+        for ( SubtreeModification mod : this.subtreeMods )
+        {
+            mod.root.rollbackForSubtree( );
+        }
+        this.subtreeMods.clear( );
+    }
+
+}
