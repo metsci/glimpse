454a7ecaf3a760d43ee9faff215c1902c0d7676b
Mike Hogye <hogye@metsci.com>
2017-07-10 11:37:13 -0400

    Clean out and deprecate NumericLabelHandlerAxisPainter
    The "ticks" field was shadowing a "ticks" field in the superclass, leading
    to very confusing behavior.
    
    This class doesn't seem to do anything useful, and seems to be left over
    from a refactoring.

--- core/src/main/java/com/metsci/glimpse/axis/painter/NumericLabelHandlerAxisPainter.java
+++ core/src/main/java/com/metsci/glimpse/axis/painter/NumericLabelHandlerAxisPainter.java
@@ -28,32 +28,16 @@ package com.metsci.glimpse.axis.painter;
 
 import com.metsci.glimpse.axis.painter.label.AxisLabelHandler;
 
+/**
+ * @deprecated Use {@link NumericAxisPainter} instead.
+ */
+@Deprecated
 public abstract class NumericLabelHandlerAxisPainter extends NumericAxisPainter
 {
-    protected AxisLabelHandler ticks;
 
     public NumericLabelHandlerAxisPainter( AxisLabelHandler ticks )
     {
         super( ticks );
-
-        this.ticks = ticks;
     }
 
-    @Override
-    public void setAxisLabel( String label )
-    {
-        this.ticks.setAxisLabel( label );
-    }
-
-    @Override
-    public AxisLabelHandler getTickCalculator( )
-    {
-        return ticks;
-    }
-
-    @Override
-    public void setTickCalculator( AxisLabelHandler ticks )
-    {
-        this.ticks = ticks;
-    }
 }
