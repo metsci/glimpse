b01cb7083139e46be3bccec355c8a4cdcd908ce6
Thomas Furlong <furlong@metsci.com>
2017-10-23 09:51:38 -0400

    Add wizard error 'Good', indicate no errors detected
    - Sort WizardErrorTypes from best to worst by ordinal for easy
      comparison

--- wizard/src/main/java/com/metsci/glimpse/wizard/WizardErrorType.java
+++ wizard/src/main/java/com/metsci/glimpse/wizard/WizardErrorType.java
@@ -26,7 +26,6 @@
  */
 package com.metsci.glimpse.wizard;
 
-
 import java.util.Collection;
 
 import javax.swing.ImageIcon;
@@ -35,46 +34,44 @@ import com.metsci.glimpse.docking.DockingUtils;
 
 public enum WizardErrorType
 {
-    Error( DockingUtils.requireIcon( "icons/fugue-icon/exclamation-small-red.png" ), DockingUtils.requireIcon( "icons/fugue-icon/exclamation-red.png" ) ),
-    Warning( DockingUtils.requireIcon( "icons/fugue-icon/exclamation-small.png" ), DockingUtils.requireIcon( "icons/fugue-icon/exclamation.png" ) ),
-    Info( DockingUtils.requireIcon( "icons/fugue-icon/exclamation-small-white.png" ), DockingUtils.requireIcon( "icons/fugue-icon/exclamation-white.png" ) );
-    
+    Good(DockingUtils.requireIcon( "icons/fugue-icon/tick-small-circle.png" ), DockingUtils.requireIcon( "icons/fugue-icon/tick-circle.png" )),
+    Info(DockingUtils.requireIcon( "icons/fugue-icon/exclamation-small-white.png" ), DockingUtils.requireIcon( "icons/fugue-icon/exclamation-white.png" )),
+    Warning(DockingUtils.requireIcon( "icons/fugue-icon/exclamation-small.png" ), DockingUtils.requireIcon( "icons/fugue-icon/exclamation.png" )),
+    Error(DockingUtils.requireIcon( "icons/fugue-icon/exclamation-small-red.png" ), DockingUtils.requireIcon( "icons/fugue-icon/exclamation-red.png" ));
+
     private ImageIcon smallIcon;
     private ImageIcon largeIcon;
-    
+
     private WizardErrorType( ImageIcon smallIcon, ImageIcon largeIcon )
     {
         this.smallIcon = smallIcon;
         this.largeIcon = largeIcon;
     }
-    
+
     public ImageIcon getSmallIcon( )
     {
         return this.smallIcon;
     }
-    
+
     public ImageIcon getLargeIcon( )
     {
         return this.largeIcon;
     }
-    
+
+    public boolean isEqualOrWorse( WizardErrorType error )
+    {
+        return this.ordinal( ) >= error.ordinal( );
+    }
+
     public static WizardErrorType getMaxSeverity( Collection<WizardError> errors )
     {
-        WizardErrorType maxType = null;
+        WizardErrorType maxType = Good;
 
         for ( WizardError error : errors )
         {
-            if ( error.getType( ) == WizardErrorType.Error )
+            if ( error.getType( ).ordinal( ) > maxType.ordinal( ) )
             {
-                return WizardErrorType.Error;
-            }
-            else if ( error.getType( ) == WizardErrorType.Warning )
-            {
-                maxType = WizardErrorType.Warning;
-            }
-            else if ( maxType == null )
-            {
-                maxType = WizardErrorType.Info;
+                maxType = error.getType( );
             }
         }
 
