52a17f9734ac10febb8185723627e74be004dfa2
Brandon Borkholder <borkholder@metsci.com>
2018-06-14 14:06:21 -0400

    Make sure to unroll the longitude to avoid jumps

--- extras-charts/src/main/java/com/metsci/glimpse/charts/shoreline/ShorelineTiler.java
+++ extras-charts/src/main/java/com/metsci/glimpse/charts/shoreline/ShorelineTiler.java
@@ -43,24 +43,26 @@ public class ShorelineTiler
         setTerseConsoleLogger( Level.FINE );
 
         LandBox box = new LandBox( 90, -90, -180, 180, false );
-        GshhsFile f = new GshhsFile( new File( GlimpseDataPaths.glimpseUserDataDir, "gshhs/gshhs_i.b" ), box, PolygonType.land );
+        GshhsFile f = new GshhsFile( new File( GlimpseDataPaths.glimpseUserDataDir, "gshhs/gshhs_l.b" ), box, PolygonType.land );
         LandShape shape = f.toShape( );
 
         File file = new File( "/home/borkholder/Desktop/tmp.bin" );
         DataOutputStream o = new DataOutputStream( new BufferedOutputStream( new FileOutputStream( file ) ) );
 
-        int tileWidth = 15;
+        int tileWidth = 20;
         int tileHeight = 10;
         for ( int lon = -180; lon < 180; lon += tileWidth )
         {
             for ( int lat = -90; lat < 90; lat += tileHeight )
             {
-                Rectangle2D bounds = new Rectangle2D.Double( lon - 1, lat - 1, tileWidth + 2, tileHeight + 2 );
-                logInfo( LOGGER, "Tesselating tile for lat =[%f,%f], lon=[%f,%f]", bounds.getMinY( ), bounds.getMaxY( ), bounds.getMinX( ), bounds.getMaxX( ) );
+                Rectangle2D bounds0 = new Rectangle2D.Double( lon - 1, lat - 1, tileWidth + 2, tileHeight + 2 );
+                Rectangle2D bounds1 = new Rectangle2D.Double( lon - 1 - 360, lat - 1, tileWidth + 2, tileHeight + 2 );
+                Rectangle2D bounds2 = new Rectangle2D.Double( lon - 1 + 360, lat - 1, tileWidth + 2, tileHeight + 2 );
+                logInfo( LOGGER, "Tesselating tile for lat =[%f,%f], lon=[%f,%f]", bounds0.getMinY( ), bounds0.getMaxY( ), bounds0.getMinX( ), bounds0.getMaxX( ) );
 
                 Collection<float[]> tess = shape.getSegments( ).stream( ).parallel( )
                         .map( s -> toArea( s ) )
-                        .map( s -> tile( bounds, s ) )
+                        .map( s -> tile( s, bounds0, bounds1, bounds2 ) )
                         .filter( a -> !a.isEmpty( ) )
                         .flatMap( s -> split( s ) )
                         .map( s -> tesselate( s ) )
@@ -73,10 +75,10 @@ public class ShorelineTiler
                     continue;
                 }
 
-                o.writeFloat( ( float ) toRadians( bounds.getMinY( ) ) );
-                o.writeFloat( ( float ) toRadians( bounds.getMaxY( ) ) );
-                o.writeFloat( ( float ) toRadians( bounds.getMinX( ) ) );
-                o.writeFloat( ( float ) toRadians( bounds.getMaxX( ) ) );
+                o.writeFloat( ( float ) toRadians( bounds0.getMinY( ) ) );
+                o.writeFloat( ( float ) toRadians( bounds0.getMaxY( ) ) );
+                o.writeFloat( ( float ) toRadians( bounds0.getMinX( ) ) );
+                o.writeFloat( ( float ) toRadians( bounds0.getMaxX( ) ) );
                 o.writeInt( tess.size( ) );
                 for ( float[] verts : tess )
                 {
@@ -95,17 +97,33 @@ public class ShorelineTiler
 
     static Area toArea( LandSegment segment )
     {
+        double lastLon = 0;
+
         Path2D.Double p = new Path2D.Double( Path2D.WIND_NON_ZERO );
         for ( int i = 0; i < segment.vertices.size( ); i++ )
         {
             LandVertex v = segment.vertices.get( i );
+            double x = v.lon;
+            double y = v.lat;
+
+            // unroll the longitude so it doesn't wrap
+            while ( x - lastLon > 180 )
+            {
+                x -= 360;
+            }
+            while ( lastLon - x > 180 )
+            {
+                x += 360;
+            }
+            lastLon = x;
+
             if ( i == 0 )
             {
-                p.moveTo( v.lon, v.lat );
+                p.moveTo( x, y );
             }
             else
             {
-                p.lineTo( v.lon, v.lat );
+                p.lineTo( x, y );
             }
         }
         p.closePath( );
@@ -113,36 +131,47 @@ public class ShorelineTiler
         return new Area( p );
     }
 
-    static Area tile( Rectangle2D bounds, Area shape )
+    static Area tile( Area shape, Rectangle2D... bounds )
     {
-        shape = ( Area ) shape.clone( );
-        shape.intersect( new Area( bounds ) );
-        return shape;
+        for ( Rectangle2D b : bounds )
+        {
+            if ( shape.intersects( b ) )
+            {
+                Area a = new Area( b );
+                a.intersect( shape );
+                if ( !a.isEmpty( ) )
+                {
+                    return a;
+                }
+            }
+        }
+
+        return new Area( );
     }
 
-    static Stream<Shape> split( Shape s )
+    static Stream<Loop> split( Shape s )
     {
-        List<Shape> all = new ArrayList<>( );
+        List<Loop> all = new ArrayList<>( );
 
         double[] v = new double[2];
         PathIterator itr = s.getPathIterator( null, 10 );
 
-        Path2D.Double p = new Path2D.Double( );
+        LoopBuilder bldr = null;
         while ( !itr.isDone( ) )
         {
             switch ( itr.currentSegment( v ) )
             {
                 case PathIterator.SEG_MOVETO:
-                    p.moveTo( v[0], v[1] );
-                    break;
+                    bldr = new LoopBuilder( );
                 case PathIterator.SEG_LINETO:
-                    p.lineTo( v[0], v[1] );
+                    bldr.addVertices( v, 1 );
                     break;
                 case PathIterator.SEG_CLOSE:
-                    p.closePath( );
-                    all.add( p );
-                    p = new Path2D.Double( );
+                    all.add( bldr.complete( Interior.onLeft ) );
+                    bldr = null;
                     break;
+                default:
+                    throw new AssertionError( );
             }
 
             itr.next( );
@@ -151,19 +180,8 @@ public class ShorelineTiler
         return all.stream( );
     }
 
-    static float[] tesselate( Shape s )
+    static float[] tesselate( Loop loop )
     {
-        double[] v = new double[2];
-        LoopBuilder bldr = new LoopBuilder( );
-        PathIterator itr = s.getPathIterator( null );
-        while ( itr.currentSegment( v ) != PathIterator.SEG_CLOSE )
-        {
-            bldr.addVertices( v, 1 );
-            itr.next( );
-        }
-
-        Loop loop = bldr.complete( Interior.onLeft );
-
         Polygon p = new Polygon( );
         p.add( loop );
         SimpleVertexAccumulator accum = new SimpleVertexAccumulator( );
