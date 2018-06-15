bfa2a1b1083e9d3ce54faff6057565339f23fd3a
Brandon Borkholder <borkholder@metsci.com>
2018-06-15 15:32:41 -0400

    Split the tiler tasking more broadly to use all threads

--- extras-charts/src/main/java/com/metsci/glimpse/charts/shoreline/ShorelineTiler.java
+++ extras-charts/src/main/java/com/metsci/glimpse/charts/shoreline/ShorelineTiler.java
@@ -21,6 +21,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.Stream.Builder;
 
 import com.metsci.glimpse.charts.shoreline.gshhs.GshhsFile;
 import com.metsci.glimpse.charts.shoreline.gshhs.GshhsPolygonHeader.PolygonType;
@@ -33,6 +34,7 @@ import com.metsci.glimpse.support.polygon.PolygonTessellator;
 import com.metsci.glimpse.support.polygon.PolygonTessellator.TessellationException;
 import com.metsci.glimpse.support.polygon.SimpleVertexAccumulator;
 import com.metsci.glimpse.util.GlimpseDataPaths;
+import com.metsci.glimpse.util.units.Length;
 
 public class ShorelineTiler
 {
@@ -43,56 +45,101 @@ public class ShorelineTiler
         setTerseConsoleLogger( Level.FINE );
 
         LandBox box = new LandBox( 90, -90, -180, 180, false );
-        GshhsFile f = new GshhsFile( new File( GlimpseDataPaths.glimpseUserDataDir, "gshhs/gshhs_l.b" ), box, PolygonType.land );
-        LandShape shape = f.toShape( );
 
-        File file = new File( "/home/borkholder/Desktop/tmp.bin" );
-        DataOutputStream o = new DataOutputStream( new BufferedOutputStream( new FileOutputStream( file ) ) );
+        File destFile = new File( "/home/borkholder/Desktop/tmp.bin" );
+        DataOutputStream o = new DataOutputStream( new BufferedOutputStream( new FileOutputStream( destFile ) ) );
+        // version
+        o.writeByte( 0 );
 
-        int tileWidth = 20;
-        int tileHeight = 10;
-        for ( int lon = -180; lon < 180; lon += tileWidth )
-        {
-            for ( int lat = -90; lat < 90; lat += tileHeight )
-            {
-                Rectangle2D bounds0 = new Rectangle2D.Double( lon - 1, lat - 1, tileWidth + 2, tileHeight + 2 );
-                Rectangle2D bounds1 = new Rectangle2D.Double( lon - 1 - 360, lat - 1, tileWidth + 2, tileHeight + 2 );
-                Rectangle2D bounds2 = new Rectangle2D.Double( lon - 1 + 360, lat - 1, tileWidth + 2, tileHeight + 2 );
-                logInfo( LOGGER, "Tesselating tile for lat =[%f,%f], lon=[%f,%f]", bounds0.getMinY( ), bounds0.getMaxY( ), bounds0.getMinX( ), bounds0.getMaxX( ) );
+        o.writeInt( 4 );
+        o.writeDouble( Length.fromNauticalMiles( 10 ) );
+        o.writeDouble( Length.fromNauticalMiles( 100 ) );
+        o.writeDouble( Length.fromNauticalMiles( 500 ) );
+        o.writeDouble( Length.fromNauticalMiles( 1000 ) );
 
-                Collection<float[]> tess = shape.getSegments( ).stream( ).parallel( )
-                        .map( s -> toArea( s ) )
-                        .map( s -> tile( s, bounds0, bounds1, bounds2 ) )
-                        .filter( a -> !a.isEmpty( ) )
-                        .flatMap( s -> split( s ) )
-                        .map( s -> tesselate( s ) )
-                        .collect( Collectors.toList( ) );
-                long nVerts = tess.stream( ).mapToLong( l -> l.length ).sum( ) / 2;
-                logInfo( LOGGER, "Found %,d polygons in tile with %,d vertices", tess.size( ), nVerts );
+        File gshhsF = new File( GlimpseDataPaths.glimpseUserDataDir, "gshhs/gshhs_f.b" );
+        LandShape land = new GshhsFile( gshhsF, box, PolygonType.land ).toShape( );
+        write( o, land, 0, 2, 1 );
 
-                if ( tess.isEmpty( ) )
-                {
-                    continue;
-                }
+        File gshhsH = new File( GlimpseDataPaths.glimpseUserDataDir, "gshhs/gshhs_h.b" );
+        land = new GshhsFile( gshhsH, box, PolygonType.land ).toShape( );
+        write( o, land, 1, 5, 3 );
 
-                o.writeFloat( ( float ) toRadians( bounds0.getMinY( ) ) );
-                o.writeFloat( ( float ) toRadians( bounds0.getMaxY( ) ) );
-                o.writeFloat( ( float ) toRadians( bounds0.getMinX( ) ) );
-                o.writeFloat( ( float ) toRadians( bounds0.getMaxX( ) ) );
-                o.writeInt( tess.size( ) );
-                for ( float[] verts : tess )
-                {
-                    o.writeInt( verts.length );
-                    for ( int i = 0; i < verts.length; i += 2 )
+        File gshhsI = new File( GlimpseDataPaths.glimpseUserDataDir, "gshhs/gshhs_i.b" );
+        land = new GshhsFile( gshhsI, box, PolygonType.land ).toShape( );
+        write( o, land, 2, 20, 10 );
+
+        File gshhsL = new File( GlimpseDataPaths.glimpseUserDataDir, "gshhs/gshhs_l.b" );
+        land = new GshhsFile( gshhsL, box, PolygonType.land ).toShape( );
+        write( o, land, 3, 30, 10 );
+
+        o.close( );
+    }
+
+    static void write( DataOutputStream out, LandShape land, int level, int tileWidth_DEG, int tileHeight_DEG ) throws IOException
+    {
+        createTiles( tileWidth_DEG, tileHeight_DEG ).parallel( )
+                .forEach( b -> {
+                    Collection<float[]> tess = land.getSegments( ).stream( ).parallel( )
+                            .map( s -> toArea( s ) )
+                            .map( s -> tile( s, b ) )
+                            .filter( a -> !a.isEmpty( ) )
+                            .flatMap( s -> split( s ) )
+                            .map( s -> tesselate( s ) )
+                            .collect( Collectors.toList( ) );
+
+                    if ( tess.isEmpty( ) )
                     {
-                        o.writeFloat( ( float ) toRadians( verts[i + 1] ) );
-                        o.writeFloat( ( float ) toRadians( verts[i] ) );
+                        return;
                     }
-                }
+
+                    try
+                    {
+                        Rectangle2D bounds0 = b[0];
+                        long nVerts = tess.stream( ).mapToLong( l -> l.length ).sum( ) / 2;
+                        logInfo( LOGGER, "Found %,d polygons in tile level %d with %,d vertices", tess.size( ), level, nVerts );
+
+                        synchronized ( out )
+                        {
+                            out.writeByte( ( byte ) level );
+                            out.writeFloat( ( float ) toRadians( bounds0.getMinY( ) ) );
+                            out.writeFloat( ( float ) toRadians( bounds0.getMaxY( ) ) );
+                            out.writeFloat( ( float ) toRadians( bounds0.getMinX( ) ) );
+                            out.writeFloat( ( float ) toRadians( bounds0.getMaxX( ) ) );
+                            out.writeInt( tess.size( ) );
+                            for ( float[] verts : tess )
+                            {
+                                out.writeInt( verts.length );
+                                for ( int i = 0; i < verts.length; i += 2 )
+                                {
+                                    out.writeFloat( ( float ) toRadians( verts[i + 1] ) );
+                                    out.writeFloat( ( float ) toRadians( verts[i] ) );
+                                }
+                            }
+                        }
+                    }
+                    catch ( IOException ex )
+                    {
+                        throw new RuntimeException( ex );
+                    }
+                } );
+    }
+
+    static Stream<Rectangle2D[]> createTiles( int tileWidth_DEG, int tileHeight_DEG )
+    {
+        Builder<Rectangle2D[]> bldr = Stream.builder( );
+        for ( int lon = -180; lon < 180; lon += tileWidth_DEG )
+        {
+            for ( int lat = -90; lat < 90; lat += tileHeight_DEG )
+            {
+                Rectangle2D bounds0 = new Rectangle2D.Double( lon - 1, lat - 1, tileWidth_DEG + 2, tileHeight_DEG + 2 );
+                Rectangle2D bounds1 = new Rectangle2D.Double( lon - 1 - 360, lat - 1, tileWidth_DEG + 2, tileHeight_DEG + 2 );
+                Rectangle2D bounds2 = new Rectangle2D.Double( lon - 1 + 360, lat - 1, tileWidth_DEG + 2, tileHeight_DEG + 2 );
+                bldr.add( new Rectangle2D[] { bounds0, bounds1, bounds2 } );
             }
         }
 
-        o.close( );
+        return bldr.build( );
     }
 
     static Area toArea( LandSegment segment )
