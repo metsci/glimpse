0da823e47ba2cfb1361deb496444a331664e0f78
Mike Hogye <hogye@metsci.com>
2018-08-31 17:34:50 -0400

    Drop type param from ViewPlacementRule, to allow lambda-syntax impls
    Java does not allow lambda syntax for interfaces with type params. In this
    case, the lambda syntax is more important than the type checking.

--- docking/src/main/java/com/metsci/glimpse/docking/group/ViewPlacementRule.java
+++ docking/src/main/java/com/metsci/glimpse/docking/group/ViewPlacementRule.java
@@ -8,6 +8,23 @@ import com.metsci.glimpse.docking.xml.GroupArrangement;
 public interface ViewPlacementRule
 {
 
-    <T> T placeView( GroupArrangement planArr, Set<String> existingViewIds, ViewPlacerMultiframe<T> placer );
+    /**
+     * <strong>NOTE:</strong> This method should really be declared with a type
+     * parameter, but lambda syntax is not allowed when there are type parameters.
+     * <p>
+     * Implementations should make exactly one call to a {@code placer} method,
+     * and return the value that the {@code placer} method returned. For example:
+     * <pre>
+     * <code>
+     * ViewPlacementRule rule = ( planArr, existingViewIds, placer ) ->
+     * {
+     *     DockerArrangementTile tile = ...;
+     *     int viewNum = ...;
+     *     return placer.addToTile( tile, viewNum );
+     * };
+     * </code>
+     * </pre>
+     */
+    Object placeView( GroupArrangement planArr, Set<String> existingViewIds, ViewPlacerMultiframe<?> placer );
 
 }
