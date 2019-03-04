9df6949138d47094f8df6905dfcafed418ce4873
Mike Hogye <hogye@metsci.com>
2019-03-04 10:53:58 -0500

    Test that ListenableSet de-duplicates dependencies

--- var2/src/test/java/com/metsci/glimpse/var2/VarTest.java
+++ var2/src/test/java/com/metsci/glimpse/var2/VarTest.java
@@ -2,6 +2,8 @@ package com.metsci.glimpse.var2;
 
 import static com.metsci.glimpse.var2.VarTestUtils.f;
 import static com.metsci.glimpse.var2.VarUtils.addOldNewListener;
+import static com.metsci.glimpse.var2.VarUtils.listenablePair;
+import static com.metsci.glimpse.var2.VarUtils.mapValueVar;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -9,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.ImmutableMap;
 
 class VarTest
 {
@@ -24,6 +28,8 @@ class VarTest
             fs.add( f( ongoing, vOld, vNew ) );
         } );
 
+        // When we make a non-ongoing change, the listener should
+        // run, even though the value hasn't changed
         a.set(  true, "A" );
         a.set( false, "A" );
 
@@ -43,6 +49,9 @@ class VarTest
             fs.add( f( ongoing, a.v( ) ) );
         } );
 
+        // The last change in this sequence should cause the listener
+        // to run, even though it sets the same value as the previous
+        // ongoing change
         a.set(  true, "A" );
         a.set( false, "B" );
         a.set(  true, "A" );
@@ -51,6 +60,31 @@ class VarTest
                               f( false, "B" ),
                               f(  true, "A" ) ),
                       fs );
+    }
+
+    @Test
+    void listenableSetShouldDedupeDeps( )
+    {
+        Var<ImmutableMap<String,String>> map1 = new VarBasic<>( ImmutableMap.of( ) );
+        Var<ImmutableMap<String,String>> map2 = new VarBasic<>( ImmutableMap.of( ) );
+        Var<String> key = new VarBasic<>( "x" );
+
+        Var<String> value1 = mapValueVar( map1, key );
+        Var<String> value2 = mapValueVar( map2, key );
+
+        ListenablePair eitherValue = listenablePair( value1, value2 );
+
+        List<Object> fs = new ArrayList<>( );
+        eitherValue.addListener( ( ) ->
+        {
+            fs.add( new Object( ) );
+        } );
+
+        // When we set the key, the listener should only run once,
+        // even though both value1 and value2 depend on key
+        key.set( "A" );
+
+        assertEquals( 1, fs.size( ) );
     }
 
 }
