a7cd1533af7b96d88e32ca349bae6b40e9767c32
Mike Hogye <hogye@metsci.com>
2020-02-07 10:45:36 -0500

    Reorganize font resources

--- core/src/main/java/com/metsci/glimpse/core/support/font/FontUtils.java
+++ core/src/main/java/com/metsci/glimpse/core/support/font/FontUtils.java
@@ -110,22 +110,22 @@ public class FontUtils
 
     public static Font getBitstreamVeraSansPlain( float size )
     {
-        return loadTrueTypeFont( FontUtils.class.getResource( "/com/metsci/glimpse/core/fonts/bitstream/Vera.ttf" ), size, Font.PLAIN );
+        return loadTrueTypeFont( FontUtils.class.getResource( "bitstream/Vera.ttf" ), size, Font.PLAIN );
     }
 
     public static Font getBitstreamVeraSansBold( float size )
     {
-        return loadTrueTypeFont( FontUtils.class.getResource( "/com/metsci/glimpse/core/fonts/bitstream/VeraBd.ttf" ), size, Font.BOLD );
+        return loadTrueTypeFont( FontUtils.class.getResource( "bitstream/VeraBd.ttf" ), size, Font.BOLD );
     }
 
     public static Font getBitstreamVeraSansItalic( float size )
     {
-        return loadTrueTypeFont( FontUtils.class.getResource( "/com/metsci/glimpse/core/fonts/bitstream/VeraIt.ttf" ), size, Font.ITALIC );
+        return loadTrueTypeFont( FontUtils.class.getResource( "bitstream/VeraIt.ttf" ), size, Font.ITALIC );
     }
 
     public static Font getBitstreamVeraSansBoldItalic( float size )
     {
-        return loadTrueTypeFont( FontUtils.class.getResource( "/com/metsci/glimpse/core/fonts/bitstream/Veralt.ttf" ), size, Font.ITALIC | Font.BOLD );
+        return loadTrueTypeFont( FontUtils.class.getResource( "bitstream/Veralt.ttf" ), size, Font.ITALIC | Font.BOLD );
     }
 
     /**
@@ -139,22 +139,22 @@ public class FontUtils
 
     public static Font getSilkscreenPlain( )
     {
-        return loadTrueTypeFont( FontUtils.class.getResource( "/com/metsci/glimpse/core/fonts/silkscreen/slkscr.ttf" ), 8, Font.PLAIN );
+        return loadTrueTypeFont( FontUtils.class.getResource( "silkscreen/slkscr.ttf" ), 8, Font.PLAIN );
     }
 
     public static Font getSilkscreenBold( )
     {
-        return loadTrueTypeFont( FontUtils.class.getResource( "/com/metsci/glimpse/core/fonts/silkscreen/slkscrb.ttf" ), 8, Font.BOLD );
+        return loadTrueTypeFont( FontUtils.class.getResource( "silkscreen/slkscrb.ttf" ), 8, Font.BOLD );
     }
 
     public static Font getSilkscreenItalic( )
     {
-        return loadTrueTypeFont( FontUtils.class.getResource( "/com/metsci/glimpse/core/fonts/silkscreen/slkscre.ttf" ), 8, Font.ITALIC );
+        return loadTrueTypeFont( FontUtils.class.getResource( "silkscreen/slkscre.ttf" ), 8, Font.ITALIC );
     }
 
     public static Font getSilkscreenBoldItalic( )
     {
-        return loadTrueTypeFont( FontUtils.class.getResource( "/com/metsci/glimpse/core/fonts/silkscreen/slkscreb.ttf" ), 8, Font.ITALIC | Font.BOLD );
+        return loadTrueTypeFont( FontUtils.class.getResource( "silkscreen/slkscreb.ttf" ), 8, Font.ITALIC | Font.BOLD );
     }
 
     public static Font getVerdanaPlain( float size )
